45f539b412ce5715c2bd2b60f149c64bf013bbc2
Vimal Kaiprath <vkaiprath@ibizsoftinc.com>
2024-03-28 16:13:43 +0530

    first commit

---
+++ springbootproject/src/main/java/com/vk/springbootproject/SpringBootProjectsApplication.java
@@ -0,0 +1,15 @@
+package com.vk.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootProjectsApplication.class, args);
+		System.out.println("Hello Spring Boot");
+	}
+
+}
---
+++ springbootproject/src/test/java/com/vk/springbootproject/SpringBootProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
